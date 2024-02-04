5173b9dda3beb8d50ddb11d3f0381090ef9264a5
KaranoSoraa <kozlov.2013@mail.ru>
2024-02-04 04:01:35 +0300

    hw_up

--- HW9.1.py
+++ HW9.1.py
@@ -1,5 +1,5 @@
 # https://github.com/KaranoSoraa/Study_py_1/tree/master
 n = int(input("Введите количество символов: "))
-lis = list(map(int, input(f"Введите {n} символов через пробел: ").split()))[::n]
-se = set(lis)
-print(f"Количество уникальных чисел: {len(se)}")
+res = set(list(map(int, input(f"Введите {n} символов через пробел: ").split()))[:n:])
+
+print(f"Количество уникальных чисел: {len(res)}")
--- HW9.2.py
+++ HW9.2.py
@@ -2,9 +2,9 @@ import random
 # https://github.com/KaranoSoraa/Study_py_1/tree/master
 li = []
 li2 = []
-for i in range(random.randint(1,10)):
+for i in range(random.randint(1, 10)):
     li.append((random.randint(0, 10)))
-    li2.append((random.randint(0,10)))
+    li2.append((random.randint(0, 10)))
 print(set(li))
 print(set(li2))
 

96cbbe6d9e245c42335aa3668a12957554615a77
KaranoSoraa <kozlov.2013@mail.ru>
2024-07-01 15:11:28 +0300

    fix

--- HW16.2.py
+++ HW16.2.py
@@ -27,15 +27,15 @@ class Turtle(object):
             print("У ее есть мечта! Найти работу в этой сфере и стать хорошим сотрудником! Она не остановится!!!")
 
     def count_moves(self, x2, y2):
-        resx = self.pos_x - x2
-        resy = self.pos_y - y2
+
+        resx = (self.pos_x - x2) / self.step
+        resy = (self.pos_y - y2) / self.step
         if resx > 0 and resy > 0:
-            return print(f"Вам нужно совершить {resx + resy} действий.")
+            return print(f"Вам нужно совершить {int(resx + resy)} действий.")
         else:
             if resx < 0:
                 resx *= -1
             if resy < 0:
                 resy *= -1
-            return print(f"Вам нужно совершить {resx + resy} действий.")
+            return print(f"Вам нужно совершить {int(resx + resy)} действий.")
        
-
--- HW8.3.py
+++ HW8.3.py
@@ -1,16 +1,16 @@
 # https://github.com/KaranoSoraa/Study_py_1/tree/master
 weight = int(input("Введите вес, который может выдержать лодка: "))
 fish = int(input("Введите количество рыбаков: "))
-we_fish = [] # массив для веса рыбаков
+we_fish = []
+
 for i in range(fish):
     weight_fish = int(input(f"Введите вес рыбака номер {i + 1}: "))
     we_fish.append(weight_fish)
+
 que = 0
 
-for i in range(len(we_fish) - 1):
-    if i > len(we_fish) - 1:
-        break
-    if len(we_fish) - i > 1 and i >= 0:
+for i in range(len(we_fish)):
+    if len(we_fish) - i > 1:
         if we_fish[i] + we_fish[i + 1] <= weight:
             print("Два рыбака смогли переправиться.")
             que += 1
@@ -18,11 +18,15 @@ for i in range(len(we_fish) - 1):
         elif we_fish[i] <= weight:
             print("Один рыбак смог переправиться")
             que += 1
-        else:
+        elif we_fish[i] > weight:
             print("Кому-то пора худеть.")
-    else:
-        que += 1
-        print("Последний убыл.")
+    elif len(we_fish) - i == 1:
+        if we_fish[i] <= weight:
+            print("Один рыбак смог переправиться")
+            que += 1
+        elif we_fish[i] > weight:
+            print("Кому-то пора худеть.")
+
 fin = ''
 if que == 1:
     fin = "лодка"
@@ -30,5 +34,6 @@ elif (que >= 2) and (que < 5):
     fin = "лодки"
 elif que >= 5:
     fin = "лодок"
+
 print(f"Для переправки всех рыбаков было использовано {que} {fin}")
 
